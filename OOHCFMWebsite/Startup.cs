93fea0daf580cf86f40ae36a0c6d062983f47103
Chris <chris.gurrell@blacklightsoftware.com>
2018-12-03 20:49:17 +0000

    Add project files.

---
+++ OOHCFMWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OOHCFMWebsite.Startup))]
+namespace OOHCFMWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
